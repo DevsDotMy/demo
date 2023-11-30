68f040f032a63ca58fa8df7cca954947d2b33e52
harishashim <harishashim@gmail.com>
2023-11-30 20:26:30 +0800

    Commit test

---
+++ src/test/java/com/example/demo/TestTest.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void setAge() {
+    }
+}
